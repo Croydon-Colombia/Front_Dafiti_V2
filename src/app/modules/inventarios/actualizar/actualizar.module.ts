7769b08e3bf61b326141883daf12806171209ad2
BrigittePadilla <153017836+BrigittePadilla@users.noreply.github.com>
2024-06-13 13:02:15 -0500

    Envío de archivo de inventario - InventoryFile

--- src/app/modules/inventarios/actualizar/actualizar.module.ts
+++ src/app/modules/inventarios/actualizar/actualizar.module.ts
@@ -2,10 +2,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActualizarService } from 'app/marketplace-api/inventory/actualizar-api';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
   ],
   declarations: [],
   providers: [ActualizarService]
